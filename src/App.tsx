5c6f0d2b70a51286381bd17a8efc1f93c9db5d50
Abdul Aleem <abdulaleem.a.official@gmail.com>
2025-05-14 23:21:55 +0530

    final most footer possibly lol fixing scroll bar existance...

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Footer from './Footer'
 function App() {
   return (
     <div className="flex flex-col min-h-screen bg-black relative overflow-hidden">
+      {/* Blurred Background */}
       <div
         className="absolute top-1/4 left-1/2 transform -translate-x-1/2 
                   w-[300px] h-[300px] blur-[100px] opacity-30 
@@ -15,16 +16,18 @@ function App() {
                   lg:w-[600px] lg:h-[600px] lg:blur-[150px]
                   bg-purple-700 pointer-events-none z-0"
       />
-        <main className="relative z-10 flex flex-col min-h-screen">  
-          <Navbar />
-          <Intro />
-          <Searchbar />
-        </main>
-      <Footer />
+
+      {/* Content Section */}
+      <div className="relative z-10 flex flex-col flex-grow">
+        <Navbar />
+        <Intro />
+        <Searchbar />
       </div>
 
-
-  )
+      {/* Footer pushed to bottom */}
+      <Footer />
+    </div>
+  );
 }
 
 export default App
