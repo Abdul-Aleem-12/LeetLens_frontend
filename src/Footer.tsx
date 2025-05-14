c9350cc6a7382daa5eb8b0422f5ffbc648541417
Abdul Aleem <abdulaleem.a.official@gmail.com>
2025-05-14 22:19:19 +0530

    footer adjusted for all devices

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,7 +1,8 @@
 const Footer = () => {
+    const year = new Date().getFullYear();
     return (
-      <footer className="w-full mt-auto bg-black text-white text-center py-6 text-sm md:text-base shadow-inner">
-        © 2025 LeetLens. All rights reserved.
+      <footer className="relative top-40 md:top-62 xl:top-73 w-full mt-auto bg-black text-white text-center py-6 text-sm md:text-base shadow-inner">
+        created by Abdul Aleem © {year} LeetLens.
       </footer>
     );
   };
