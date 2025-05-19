cf4d8822d997fddfb603304275d20d449e6cfaf0
Abdul Aleem <abdulaleem.a.official@gmail.com>
2025-05-19 21:33:25 +0530

    removed copywrite

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     const year = new Date().getFullYear();
     return (
       <footer className="w-full mt-auto bg-black text-white text-center py-6 text-sm md:text-base shadow-inner">
-        created by Abdul Aleem © {year} LeetLens.
+        created by Abdul Aleem A, LeetLens.
       </footer>
     );
   };
