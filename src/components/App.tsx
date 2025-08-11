e076b8a73c5239e29b57e1b2b8d5802685ff69be
Abdul Aleem <abdulaleem.a.official@gmail.com>
2025-08-11 21:46:39 +0530

    Production v1

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -8,10 +8,11 @@ import Analyze from "./analysis/Analyze"
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
+console.log("Hello mate, What are you doing in Dev Tab?");
 
 function App() {
   return (
-    console.log("Hello mate, What are you doing in Dev Tab?"),
+    
     <div className="bg-black relative min-h-screen overflow-hidden grad bad" >
     <main className="relative z-10 flex flex-col flex-grow">
       <Navbar />
